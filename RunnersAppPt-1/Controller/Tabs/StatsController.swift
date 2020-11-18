81bb9f68d6488234156e5fa9908cf815fe624b69
Karol <karol.korzen@icloud.com>
2020-11-18 22:37:23 +0100

    animate block for target and actual distance runned

--- RunnersAppPt-1/Controller/Tabs/StatsController.swift
+++ RunnersAppPt-1/Controller/Tabs/StatsController.swift
@@ -14,6 +14,14 @@ class StatsController: UIViewController {
     
     private let viewModel = StatsSummaryViewModel()
     private let statsLabel = Utilities.shared.infoRunLabel()
+    private var sceneTitle = UILabel()
+    private var settingsIcon = Utilities.shared.actionButton(withSystemName: "gear")
+    
+    
+    private let targetRect = UIView.init()
+    private let currentRect = UIView.init()
+    
+    let fpc = FloatingPanelController()
     
     //MARK: - Lifecycle
     
@@ -34,30 +42,75 @@ class StatsController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: animated)
+        initialStats()
+        introAnimate()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
     }
     
     //MARK: - Helpers
     
     func setFloatingPanel() {
-        let fpc = FloatingPanelController()
         fpc.delegate = self
         let trainingListController = TrainingsListController(collectionViewLayout: UICollectionViewFlowLayout())
         fpc.set(contentViewController: trainingListController)
         fpc.track(scrollView: trainingListController.collectionView)
         fpc.layout = MyStatsFloatingPanelLayout()
-        
         fpc.addPanel(toParent: self)
     }
     
     func configureUI(){
         view.backgroundColor = .white
         
-        view.addSubview(statsLabel)
-        statsLabel.layer.zPosition = CGFloat(-(Float.greatestFiniteMagnitude))
-        statsLabel.text = viewModel.testLabel
-        statsLabel.setDimensions(width: view.frame.width - 20, height: 300)
-        statsLabel.font = UIFont.italicSystemFont(ofSize: 10)
-        statsLabel.center(inView: self.view)
+        view.addSubview(sceneTitle)
+        sceneTitle.text = "Your scores"
+        sceneTitle.font = UIFont.boldSystemFont(ofSize: 26)
+        sceneTitle.textColor = .appTintColor
+        sceneTitle.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, paddingTop: 30, paddingLeft: 30)
+        sceneTitle.setDimensions(width: view.frame.width/2-20, height: 50)
+        
+        view.addSubview(settingsIcon)
+        settingsIcon.anchor(top: view.safeAreaLayoutGuide.topAnchor, right: view.rightAnchor, paddingTop: 30, paddingRight: 30)
+        settingsIcon.backgroundColor = .white
+        settingsIcon.tintColor = .appTintColor
+        settingsIcon.setDimensions(width: 50, height: 50)
+    }
+    
+    func initialStats() {
+        view.addSubview(targetRect)
+        targetRect.layer.zPosition = -2
+        targetRect.backgroundColor = .lightGray
+        let initialHeight = view.frame.height/30
+        targetRect.frame = CGRect(x: view.frame.width/4, y: view.frame.height/2-initialHeight, width: view.frame.width/5, height: initialHeight)
+        targetRect.layer.cornerRadius = 10
+        targetRect.alpha = 0.8
+        
+        view.addSubview(currentRect)
+        currentRect.layer.zPosition = -1
+        currentRect.backgroundColor = UIColor(red: 0.906, green: 0.784, blue: 0.573, alpha: 1.000)
+        currentRect.frame = CGRect(x: view.frame.width/4, y: view.frame.height/2-initialHeight, width: view.frame.width/5, height: initialHeight)
+        currentRect.layer.cornerRadius = 10
+        currentRect.alpha = 0.8
+    }
+    
+    func introAnimate() {
+        UIView.animate(withDuration: 0.5, delay: 0.2, options: .curveEaseInOut) {
+            let targetHeight = self.view.frame.height/3
+            self.targetRect.frame = CGRect(x: self.view.frame.width/4, y: self.view.frame.height/2-targetHeight, width: self.view.frame.width/5, height: targetHeight)
+            self.targetRect.layer.cornerRadius = 10
+            self.targetRect.alpha = 0.8
+            
+            let currentHeight = CGFloat((self.viewModel.statsSummary.wholeDistance / 1000.0))*self.view.frame.height/30 //FIXME: - TARGET DISTANCE HEREs
+            self.currentRect.frame = CGRect(x: self.view.frame.width/4, y: self.view.frame.height/2-currentHeight, width: self.view.frame.width/5, height: currentHeight)
+            self.currentRect.layer.cornerRadius = 10
+            self.currentRect.alpha = 1.0
+            
+        } completion: { (bool) in
+            print("done")
+        }
+
     }
 }
 
